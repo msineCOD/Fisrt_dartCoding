b3b51d0c570a55ada3e76cde844a84c44084f82b
msineCOD <m.hosine.nge2201@gmail.com>
2022-12-18 17:03:49 +0330

    lesson one

--- bin/first_dartcod.dart
+++ bin/first_dartcod.dart
@@ -1,20 +1,92 @@
 import 'package:first_dartcod/first_dartcod.dart' as first_dartcod;
 
+  enum MyEnum{buy,sell,outStore}
+
 void main(List<String> arguments) {
-  print('Hello world: ');
 
-  for (var i = 0; i <= 6; i++) {
-    // print("object");
-    
-    if (i == 4) {
-      continue;
-    }
 
-    if (i == 5) {
-      break;
-    }
-    print("${i} is :");
-  }
+  // var
+
+  // int , String , boolean , double
+
+  // int a = 10;
+  // double b = 10.2;
+  // String c = 'ali';
+  // bool d = true;
+
+  // var and dynamic
+
+  // var a = 10;
+  // a = 20;
+  //
+  // dynamic f = 20;
+  // f = 'ss';
+
+
+  // list , map
+
+  // List <String> list = ['ss','2'];
+  // List <dynamic> listB = [1,'s'];
+  // Map<String,dynamic> map = {'name':'ali','age':20};
+
+  // Set a = {'ali','akbar','ali'};
+  // print(a);
+
+  // Enum
+  // MyEnum.buy;
+
+  // list
+
+  // List list = List.filled(10, true);
+  // print(list);
+  //
+  // List listB = List.filled(5, 10,growable: true);
+  // listB.add(2);
+  // print(listB);
+  //
+  // List listC = List.generate(10, (index) {
+  //
+  // });
+
+  // List list = [1,5,3];
+  // List list2 = [2];
+  // list2.addAll(list);
+  //
+  // List list3 = [6];
+  // List listD = [...list,...list2,...list3];
+  // print(listD);
+
+  List list = [10,20,3];
+  // list.add(5);
+  // bool a =  list.any((element) => element == 2);
+  // print(a);
+  // print(list);
+  // list.insert(3, 4);
+  // print(list);
+  // list.map((e) => e).toList();
+  // list.toSet();
+
+  // list.take(1);
+  // print(list.take(2));
+
+  // list
+  //     ..add(30)
+  //     ..add(40)
+  //     ..insert(1, 90);
+  // print(list);
+
+
+  // var b;
+  // print(b ?? 0);
+  // b == null ? 0 : null;
+
+  // int? a;
+  // print(a);
+  //
+  // int? b ;
+  // print(b!);
+
+
 
 
 }
